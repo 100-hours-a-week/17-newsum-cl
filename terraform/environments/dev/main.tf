44dab6fe2dd50697dfd02473f0e40e1903f87ec9
rok.lee <guswp1128@gmail.com>
2025-06-11 23:18:39 +0900

    fix: module-sg(#127)

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -13,7 +13,7 @@ module "vpc" {
   was_subnets      = ["10.1.100.0/24", "10.1.110.0/24"] # dev-was-subnet-a, dev-was-subnet-b
   db_subnets       = ["10.1.200.0/24", "10.1.210.0/24"] # dev-db-subnet-a, dev-db-subnet-b
 
-  vpc_peering_connection_id = "dev"
+  vpc_peering_connection_id = "vpc-02cf79b85747fe43d"
 
   tags = {
     Environment = "dev"
@@ -22,66 +22,74 @@ module "vpc" {
 }
 
 # 보안그룹
+module "dev_sg" {
+  source = "../../modules/sg"
+
+  name = "dev"
+  description = "dev security group"
+  vpc_id = module.vpc.vpc_id
+  
+}
 
 # ALB
-module "alb_sg" {
-  source = "../../modules/sg"
+# module "sg" {
+#   source = "../../modules/sg"
   
-  name = "alb"
-  description = "alb security group"
-  vpc_id = module.vpc.vpc_id
-}
+#   name = "dev"
+#   description = "alb security group"
+#   vpc_id = module.vpc.vpc_id
+# }
 
-# WAS
-module "was_sg" {
-  source = "../../modules/sg" 
+# # WAS
+# module "was_sg" {
+#   source = "../../modules/sg" 
   
-  name = "was"
-  description = "was security group"
-  vpc_id = module.vpc.vpc_id
-}
+#   name = "was"
+#   description = "was security group"
+#   vpc_id = module.vpc.vpc_id
+# }
 
-# DB
-module "db_sg" {
-  source = "../../modules/sg"
+# # DB
+# module "db_sg" {
+#   source = "../../modules/sg"
   
-  name = "db"
-  description = "db security group"
-  vpc_id = module.vpc.vpc_id
-}
+#   name = "db"
+#   description = "db security group"
+#   vpc_id = module.vpc.vpc_id
+# }
 
-# Redis
-module "redis_sg" {
-  source = "../../modules/sg"
+# # Redis
+# module "redis_sg" {
+#   source = "../../modules/sg"
 
-  name = "redis"
-  description = "redis security group"
-  vpc_id = module.vpc.vpc_id
-}
+#   name = "redis"
+#   description = "redis security group"
+#   vpc_id = module.vpc.vpc_id
+# }
 
-# NAT
-module "nat_sg" {
-  source = "../../modules/sg"
+# # NAT
+# module "nat_sg" {
+#   source = "../../modules/sg"
 
-  name = "nat"
-  description = "nat security group"
-  vpc_id = module.vpc.vpc_id
-}
+#   name = "nat"
+#   description = "nat security group"
+#   vpc_id = module.vpc.vpc_id
+# }
 
-# OpenVPN
-module "openvpn_sg" {
-  source = "../../modules/sg"
+# # OpenVPN
+# module "openvpn_sg" {
+#   source = "../../modules/sg"
 
-  name = "openvpn"
-  description = "openvpn security group"
-  vpc_id = module.vpc.vpc_id
-}
+#   name = "openvpn"
+#   description = "openvpn security group"
+#   vpc_id = module.vpc.vpc_id
+# }
 
-# Kafka
-module "kafka_sg" {
-  source = "../../modules/sg"
-  
-  name = "kafka"
-  description = "kafka security group"
-  vpc_id = module.vpc.vpc_id
-}
+# # Kafka
+# module "kafka_sg" {
+#   source = "../../modules/sg"
+
+#   name = "kafka"
+#   description = "kafka security group"
+#   vpc_id = module.vpc.vpc_id
+# }
