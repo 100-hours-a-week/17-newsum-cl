e0e6b85090adf74b10b591e2cd4ae3d538744e8f
rok.lee <guswp1128@gmail.com>
2025-06-11 21:48:07 +0900

    module/sg create

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -1,4 +1,6 @@
 # /Users/roklee/NewSum/17-newsum-cl/terraform/environments/dev/main.tf
+
+# VPC 모듈
 module "vpc" {
   source = "../../modules/vpc"
   
@@ -11,8 +13,75 @@ module "vpc" {
   was_subnets      = ["10.1.100.0/24", "10.1.110.0/24"] # dev-was-subnet-a, dev-was-subnet-b
   db_subnets       = ["10.1.200.0/24", "10.1.210.0/24"] # dev-db-subnet-a, dev-db-subnet-b
 
+  vpc_peering_connection_id = "dev"
+
   tags = {
     Environment = "dev"
     Terraform   = "true"
   }
+}
+
+# 보안그룹
+
+# ALB
+module "alb_sg" {
+  source = "../../modules/sg"
+  
+  name = "alb"
+  description = "alb security group"
+  vpc_id = module.vpc.vpc_id
+}
+
+# WAS
+module "was_sg" {
+  source = "../../modules/sg" 
+  
+  name = "was"
+  description = "was security group"
+  vpc_id = module.vpc.vpc_id
+}
+
+# DB
+module "db_sg" {
+  source = "../../modules/sg"
+  
+  name = "db"
+  description = "db security group"
+  vpc_id = module.vpc.vpc_id
+}
+
+# Redis
+module "redis_sg" {
+  source = "../../modules/sg"
+
+  name = "redis"
+  description = "redis security group"
+  vpc_id = module.vpc.vpc_id
+}
+
+# NAT
+module "nat_sg" {
+  source = "../../modules/sg"
+
+  name = "nat"
+  description = "nat security group"
+  vpc_id = module.vpc.vpc_id
+}
+
+# OpenVPN
+module "openvpn_sg" {
+  source = "../../modules/sg"
+
+  name = "openvpn"
+  description = "openvpn security group"
+  vpc_id = module.vpc.vpc_id
+}
+
+# Kafka
+module "kafka_sg" {
+  source = "../../modules/sg"
+  
+  name = "kafka"
+  description = "kafka security group"
+  vpc_id = module.vpc.vpc_id
 }
